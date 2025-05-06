287ec99ac63c7941100a0398b2bdc519bd03d652
James Andrei Aguilar <jaguilar@gbox.adnu.edu.ph>
2025-05-07 01:01:49 +0800

    Nav and Header

---
+++ faraway/frontend/src/components/Nav.tsx
@@ -0,0 +1,84 @@
+import React, { useState } from 'react';
+import placeholderProfile from '../assets/placeholderProfile.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { 
+  faCircleDollarToSlot, 
+  faFilePrescription 
+} from '@fortawesome/free-solid-svg-icons';
+import { Icon } from '@iconify/react';
+
+const Nav = ({ user }) => {
+  const [patient, setPatient] = useState(user);
+
+  return (
+    <aside className='w-64 bg-[#005F92] text-white min-h-full'>
+      
+      <div className='flex flex-col justify-center items-center mt-10 mb-6'>
+        <img 
+          src={placeholderProfile} 
+          alt="placeholderProfile" 
+          className='w-24 h-24 rounded-full mb-3'
+        />
+        <h1 className='font-bold text-lg'>{patient.name}</h1>
+        <h2 className='text-sm'>Patient</h2>
+      </div>
+
+      
+      <div className='px-6'>
+        <h1 className='text-lg font-semibold ml-4 mb-2'>MAIN</h1>
+            <nav className='flex flex-col'>
+                <a 
+                href="#" 
+                className='flex items-center gap-3 px-4 py-2 rounded hover:bg-[#00446b] transition text-white'
+                >
+                <Icon icon="material-symbols:dashboard" className="w-5" />
+                <span>Dashboard</span>
+                </a>
+                <a 
+                href="#" 
+                className='flex items-center gap-3 px-4 py-2 rounded hover:bg-[#00446b] transition text-white'
+                >
+                <Icon icon="material-symbols:prescriptions" className="w-5" />
+                <span>Medical Record</span>
+                </a>
+                <a 
+                href="#" 
+                className='flex items-center gap-3 px-4 py-2 rounded hover:bg-[#00446b] transition text-white'
+                >
+                <FontAwesomeIcon icon={faCircleDollarToSlot} className="w-5" />
+                <span>Billing</span>
+                </a>
+                <a 
+                href="#" 
+                className='flex items-center gap-3 px-4 py-2 rounded hover:bg-[#00446b] transition text-white'
+                >
+                <FontAwesomeIcon icon={faFilePrescription} className="w-5" />
+                <span>Prescriptions</span>
+                </a>
+            </nav>
+        </div>
+
+        <div className='px-6 mt-10'>
+        <h1 className='text-lg font-semibold ml-4 mb-2'>SUPPORT</h1>
+            <nav className='flex flex-col'>
+                <a 
+                href="#" 
+                className='flex items-center gap-3 px-4 py-2 rounded hover:bg-[#00446b] transition text-white'
+                >
+                <Icon icon="material-symbols:dashboard" className="w-5" />
+                <span>Dashboard</span>
+                </a>
+                <a 
+                href="#" 
+                className='flex items-center gap-3 px-4 py-2 rounded hover:bg-[#00446b] transition text-white'
+                >
+                <Icon icon="material-symbols:prescriptions" className="w-5" />
+                <span>Medical Record</span>
+                </a>
+            </nav>
+        </div>
+    </aside>
+  );
+};
+
+export default Nav;
--- faraway/frontend/src/pages/Landing.tsx
+++ faraway/frontend/src/pages/Landing.tsx
@@ -1,19 +1,26 @@
 import React, { useEffect, useState } from 'react'
 import Button from '../components/Button';
+import Header from '../components/Header';
+import Nav from '../components/Nav';
 
 const Landing = ({user}) => {
   const [patient, setPatient] = useState(user);
   
+  const firstName = patient.name.split(' ')[0];
 
   return (
-    <div>
-      <h1>Welcome <span className="text-primary font-black">{patient.name}</span>!</h1>
-      <p>Email: <span className='text-primary font-black'>{patient.email}</span></p>
-      <Button onClick={() => {
-        localStorage.removeItem('token');
-        alert("Logging out....");
-        window.location.reload();
-      }} text={'Logout'} />
+    <div className='flex flex-col min-h-screen'>
+      <header>
+        <Header />
+      </header>
+      <div className='flex flex-1'>
+        <Nav user={patient} />
+        <main className='flex-1 p-6'>
+          <h1>Good Day, <span className="text-primary font-black">{firstName}</span>!</h1>
+          <p>We’re here to help you stay healthy—check in regularly to stay up to date on your health and care plan.</p>
+        </main>
+      </div>
+
     </div>
   )
 }
