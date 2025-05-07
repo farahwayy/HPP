982060d63b6c593ec7bd0fad9589b2f45614cacd
James Andrei Aguilar <jaguilar@gbox.adnu.edu.ph>
2025-05-07 18:52:00 +0800

    commit

--- faraway/frontend/src/components/Nav.tsx
+++ faraway/frontend/src/components/Nav.tsx
@@ -66,7 +66,7 @@ const Nav = ({ user }) => {
                 className='flex items-center gap-3 px-4 py-2 rounded hover:bg-[#00446b] transition text-white'
                 >
                 <Icon icon="iconamoon:shield-yes-fill" className="w-5" />
-                <span>Dashboard</span>
+                <span>Help Center</span>
                 </a>
                 <a 
                 href="#" 
