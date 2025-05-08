745e66d83665f5b9c469fbd02c0c3de6047a87eb
James Andrei Aguilar <jaguilar@gbox.adnu.edu.ph>
2025-05-08 21:20:36 +0800

    PrescriptionPage

--- faraway/frontend/src/pages/Presc.tsx
+++ faraway/frontend/src/pages/Presc.tsx
@@ -1,12 +1,16 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Header from '../components/Header';
 import Nav from '../components/Nav';
 import useDecodedToken from '../utils/DecodeToken';
 import SearchBar from '../components/SearchBar';
+import Filter from '../components/Filter';
+
+import PrescriptionTable from '@/components/PrescripTable';
 
 
 const Prescriptions = () => {
     const patient = useDecodedToken();
+    const [theme, setTheme] = useState("light");
 
     if (!patient) {
       return <div className="p-10">Loading profile...</div>;
@@ -25,6 +29,14 @@ const Prescriptions = () => {
               description={'View your Health Information'}
               data={'Prescriptions'}
             />
+          <div className='flex-col py-30 px-35'>
+            <div className='flex justify-end mb-10'>
+              <Filter value={theme} onChange={setTheme} />
+            </div>
+            <div>
+              <PrescriptionTable />
+            </div>
+          </div>
         </div>
       </main>
     </div>
