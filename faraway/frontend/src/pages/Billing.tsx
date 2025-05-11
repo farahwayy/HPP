7e85611c3d0d530ac809b3b682be782bc695fa5c
ILGeneral <160194833+ILGeneral@users.noreply.github.com>
2025-05-12 01:48:42 +0800

    Update Billing.tsx
    updated the whole page page

--- faraway/frontend/src/pages/Billing.tsx
+++ faraway/frontend/src/pages/Billing.tsx
@@ -1,29 +1,57 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from '../components/Header';
 import Nav from '../components/Nav';
 import useDecodedToken from '../utils/DecodeToken';
+import SearchBar from '../components/SearchBar';
+import Filter from '@/components/Filter';
+import { sortOptions, typeOptions } from '@/utils/filterOptions';
+import MedicalTable from '@/components/BillTable';
 
+const BillRecord = () => {
+  const [sortOrder, setSortOrder] = useState("newest");
+  const [typeFilter, setTypeFilter] = useState("all");
+  const patient = useDecodedToken();
 
-const Billing = () => {
-    const patient = useDecodedToken();
-
-    if (!patient) {
-      return <div className="p-10">Loading profile...</div>;
-    }
+  if (!patient) {
+    return <div className="p-10">Loading profile...</div>;
+  }
 
   return (
-    <div className='flex flex-col min-h-screen'>
+    <div className="flex flex-col min-h-screen">
       <header>
         <Header />
       </header>
-      <div className='flex flex-1'>
+      <div className="flex flex-1">
         <Nav user={patient} />
-        <main className='flex-1'>
-          
-        </main>
+        <div className="flex-1">
+          <SearchBar
+            title="Billing"
+            description="View and manage your account statements"
+            data="Bills"
+          />
+          <div className="flex flex-col justify-between p-15">
+            <div className="flex-col">
+              <h1 className="text-3xl font-medium">All Statements</h1>
+              <p className="text-lg font-medium text-[#9E9E9E]">
+                Click on a statement to view details
+              </p>
+            </div>
+            <div className="flex justify-end mb-10 gap-3">
+              <Filter
+                value={sortOrder}
+                onChange={setSortOrder}
+                options={sortOptions}
+                placeholder="Sort by"
+              />
+            </div>
+            <div>
+              <MedicalTable sortOrder={sortOrder} typeFilter={typeFilter} />
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
 };
 
-export default Billing;
+export default BillRecord;
