f67b3143895668968f42e02bb2744b34724e9f44
pat <158650319+pnestrella@users.noreply.github.com>
2025-05-19 04:55:23 +0800

    Update Login.tsx

--- faraway/frontend/src/utils/Login.tsx
+++ faraway/frontend/src/utils/Login.tsx
@@ -10,9 +10,9 @@ const Login = () => {
     if (token) {
       localStorage.setItem('token', token);
       console.log('Stored Successfully')
-      window.location.href='https://hppbackend.onrender.com/landing'
+      window.location.href='https://hpp-745s.onrender.com/landing'
     }else{
-        window.location.href='https://hppbackend.onrender.com/'
+        window.location.href='https://hpp-745s.onrender.com/'
     }
   },[])
 
